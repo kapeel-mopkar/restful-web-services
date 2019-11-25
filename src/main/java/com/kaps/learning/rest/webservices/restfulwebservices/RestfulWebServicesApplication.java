93175dd4874f403c59251b279605cc9083a54f9c
Kapeel Mopkar <kapeel_mopkar@persistent.com>
2019-11-25 11:24:39 +0530

    Spring security - basic auth added

--- src/main/java/com/kaps/learning/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/kaps/learning/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,12 +3,15 @@ package com.kaps.learning.rest.webservices.restfulwebservices;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
